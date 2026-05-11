8a46235562a41d2c0c407e088fe9ad3cd887e1c7
MrBroccoliJP <98jpedro@gmail.com>
2026-05-11 18:15:17 +0100

    version update

--- mementoframe/version_info.py
+++ mementoframe/version_info.py
@@ -1,5 +1,5 @@
 VERSIONS = {
-    "Frontend": "2.0.3",         # html and css files | Last Change: Fix styling on the image if its vertical, rounded corners and positioning.
+    "Frontend": "2.0.4",         # html and css files | Last Change: Fix styling on the image if its vertical, rounded corners and positioning.
     "App": "2.0.2",              # app.py             | Last Change: Documentation update 
     "Api Service": "2.0.1",      # api_service.py     | Last Change: Documentation update 
     "AP Mode Manager": "2.0.1",  # ap_mode_manager.py | Last Change: Documentation update and rework of the code logic
